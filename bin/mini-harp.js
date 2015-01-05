00406ec3f8c086b67546936cb15811b838372523
Haojian Wu <hokein.wu@gmail.com>
2015-01-05 20:17:01 +0800

    Implement mini-harp command.

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
-console.log(1);
+var miniHarp = require('../');
+var parseArgs = require('minimist');
+
+var app = miniHarp();
+var options = parseArgs(process.argv.slice(2));
+var port = options.port || 4000;
+
+console.log("Starting mini-harp on http://localhost:" + port);
+app.listen(port);
