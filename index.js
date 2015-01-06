bccd076bb05aa0eb9dc8d2957dbcb29dc4424f55
Haojian Wu <hokein.wu@gmail.com>
2015-01-06 22:51:52 +0800

    Serving root path.

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var less = require('./lib/processor/less');
 
 module.exports = function(path) {
   var app = connect();
-  app.use(serveStatic(path)).use(jade(path)).use(less(path));
+  app.use(function(req, res, next) {
+    if (req.url == '/')
+      req.url = '/index.html';
+    next();
+  }).use(serveStatic(path)).use(jade(path)).use(less(path));
   return app;
 }
