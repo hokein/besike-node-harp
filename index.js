837946320cb155ab48e7136966e008e2c1645ce9
Haojian Wu <hokein.wu@gmail.com>
2015-01-06 23:10:20 +0800

    Reject stupid requests.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 var connect = require('connect');
+var path = require('path');
 var serveStatic = require('serve-static');
 var jade = require('./lib/processor/jade');
 var less = require('./lib/processor/less');
+
+function rejuctSourceFiles(req, res, next) {
+  if (path.extname(req.url) == '.jade' ||
+      path.extname(req.url) == '.less') {
+    res.statusCode = 404;
+    res.end();
+  } else {
+    next();
+  }
+}
 
 module.exports = function(path) {
   var app = connect();
@@ -9,6 +20,6 @@ module.exports = function(path) {
     if (req.url == '/')
       req.url = '/index.html';
     next();
-  }).use(serveStatic(path)).use(jade(path)).use(less(path));
+  }).use(rejuctSourceFiles).use(serveStatic(path)).use(jade(path)).use(less(path));
   return app;
 }
