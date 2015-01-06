6c11e7352dc5cb4469d253131378f061a68050e2
Haojian Wu <hokein.wu@gmail.com>
2015-01-06 22:44:19 +0800

    Implement less template rendering.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 var jade = require('./lib/processor/jade');
+var less = require('./lib/processor/less');
 
 module.exports = function(path) {
   var app = connect();
-  app.use(serveStatic(path)).use(jade(path));
+  app.use(serveStatic(path)).use(jade(path)).use(less(path));
   return app;
 }
